069df7bb9f33b8df5b92cd6a6a9739133f4e922f
Tor Nordam <tor.nordam@sintef.no>
2020-04-03 15:10:19 +0200

    more readable version of pointertest2.f90

--- src/pointertest2.f90
+++ src/pointertest2.f90
@@ -1,90 +1,112 @@
-module mymod
+module interpolator_module
     implicit none
     integer, parameter :: WP = kind(1.0D0)
 
     interface
-        function integrand_interface(X, t) result( val )
+        ! This is the general form of the right-hand side of an ODE
+        function rhs(X, t) result( val )
             import :: WP
-            real(WP), dimension(:)       :: X
-            real(WP), dimension(size(X)) :: val
-            real(WP)                     :: t
+            real(WP), dimension(:),      intent(in) :: x
+            real(WP),                    intent(in) :: t
+            real(WP), dimension(size(x))            :: val
         end function
     end interface
 
-    type mytype
-        real(WP) :: q
-        procedure(integrand_interface), nopass, pointer :: integrand
+    type interpolator_type
+        ! This type would in practice store arrays,
+        ! of discrete data to be interpolated.
+        real(WP) :: stored_data
+        procedure(rhs), nopass, pointer :: eval
     contains
         procedure :: init
     endtype
 
-    class(mytype), pointer :: this_
+    class(interpolator_type), pointer :: interpolator
+
 contains
 
-subroutine init( this )
-    class(mytype), target :: this
-    this % integrand => myfunc
-    this_ => this
-end subroutine
+    subroutine init( this, stored_data )
+        implicit none
+        class(interpolator_type), target :: this
+        real(WP) :: stored_data
+        this % stored_data = stored_data
+        this % eval => evaluate
+        interpolator => this
+    end subroutine
 
-function myfunc(X, t) result( val )
-    real(WP), dimension(:)       :: X
-    real(WP), dimension(size(X)) :: val
-    real(WP)                     :: t
-    val = this_ % q * x
-end function
+    function evaluate(X, t) result( val )
+        implicit none
+        real(WP), dimension(:),      intent(in) :: x
+        real(WP),                    intent(in) :: t
+        real(WP), dimension(size(x))            :: val
+        ! This is where interpolation would happen
+        val = interpolator % stored_data * x
+    end function
 
 end module
 
 
 program main
-    use mymod
+    use interpolator_module, only : interpolator_type
     implicit none
-    !integer, parameter :: WP = kind(1.0D0)
-    type(mytype) :: mt
+    integer, parameter :: WP = kind(1.0D0)
+    type(interpolator_type) :: interp
     real(WP), dimension(2) :: X
     real(WP) :: t, h
 
-    call mt% init
-    mt% q = 100.0
+    ! initialise interpolator with some data
+    call interp % init(-0.1_WP)
 
-    X = (/ 0.0_WP, 1.0_WP /)
+    X = (/ 2.0_WP, 1.0_WP /)
     t = 0.0_WP
     h = 1.0_WP
 
-    call rk1(X, t, h, mt%integrand)
-    !call integrator(0.0_WP,  mt% integrand, 1.0, 2.0, ans )
+    ! Example of calling rk1 with the "type-bound procedure"
+    ! which evaluates an interpolator
+    call rk4(X, t, h, interp % eval )
+    print *, X
+
+    ! Example of calling rk1 with analytical function
+    call rk4(X, t, h, f )
     print *, X
 
     contains
-    subroutine rk1(X, t, h, f)
-        ! Makes one step with the forward Euler method.
+
+    subroutine rk4(X, t, h, f)
+        ! Makes one step with 4th-order Runge-Kutta.
         ! Calculates next position using timestep h.
-        ! See, e.g., Hairer, Nørsett and Wanner (1993, p. 51)
         implicit none
-    !    integer, parameter :: WP = kind(1.0D0)
-        ! IO
         real(WP), intent(inout), dimension(:)   :: X
         real(WP), intent(inout)                 :: t
         real(WP), intent(in)                    :: h
         interface
             function f(X, t) result(val)
                 import WP
-                real(WP), dimension(:)       :: X
-                real(WP), dimension(size(X)) :: val
-                real(WP)                     :: t
+                real(WP), dimension(:),      intent(in) :: x
+                real(WP),                    intent(in) :: t
+                real(WP), dimension(size(x))            :: val
             end function
         end interface
         ! Local variables
-        real(WP), dimension(size(X))            :: k1
+        real(WP), dimension(size(X))            :: k1, k2, k3, k4
 
         ! Evaluations of f(x, t)
-        k1 = f(X, t)
+        k1 = f(X,          t)
+        k2 = f(X + k1*h/2, t + h/2)
+        k3 = f(X + k2*h/2, t + h/2)
+        k4 = f(X + k3,     t + h)
 
         ! Next position
-        X  = X + h*k1
+        X  = X + h*(k1 + 2*k2 + 2*k3 + k4)/6
         t  = t + h
     end subroutine
 
+    pure function f(x, t) result(val)
+        implicit none
+        real(WP), dimension(:),      intent(in) :: x
+        real(WP),                    intent(in) :: t
+        real(WP), dimension(size(x))            :: val
+        val = -0.1_WP*x
+    end function
 
 end program
